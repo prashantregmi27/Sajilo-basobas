50b26feafeba5426a61cb2c97f7fa175923a379b
prashantregmi27 <prashantregmi27@gmail.com>
2024-08-08 13:21:23 +0545

    header-change

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
     const urlParams = new URLSearchParams(window.location.search);
     urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
-    navigate(`search?${searchQuery}`);
+    navigate(`/search?${searchQuery}`);
   };
 
   useEffect(() => {
@@ -27,8 +27,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sajilo</span>
-            <span className="text-slate-700">Basobas</span>
+            <span className="text-slate-500">Sahand</span>
+            <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
         <form
@@ -46,19 +46,18 @@ export default function Header() {
             <FaSearch className="text-slate-600" />
           </button>
         </form>
-
         <ul className="flex gap-4">
           <Link to="/">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               Home
             </li>
           </Link>
-          <Link to="/About">
+          <Link to="/about">
             <li className="hidden sm:inline text-slate-700 hover:underline">
               About
             </li>
           </Link>
-          <Link to="/Profile">
+          <Link to="/profile">
             {currentUser ? (
               <img
                 className="rounded-full h-7 w-7 object-cover"
@@ -66,9 +65,7 @@ export default function Header() {
                 alt="profile"
               />
             ) : (
-              <li className="hidden sm:inline text-slate-700 hover:underline">
-                Sign in
-              </li>
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
             )}
           </Link>
         </ul>
